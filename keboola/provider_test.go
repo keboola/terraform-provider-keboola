efb43d327ff3888e807c77dd797d2ed31096a4c5
Tomas Kacur <tomaskacur@Tomass-MacBook-Pro.local>
2023-03-05 12:00:25 +0100

    migrate terraform plugin framework to v1.0.0

--- keboola/provider_test.go
+++ keboola/provider_test.go
@@ -2,6 +2,7 @@ package keboola
 
 import (
 	"os"
+	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
@@ -24,6 +25,12 @@ var (
 	// CLI command executed to create a provider server to which the CLI can
 	// reattach.
 	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
-		"keboola": providerserver.NewProtocol6WithError(New()),
+		"keboola": providerserver.NewProtocol6WithError(New("test")()),
 	}
 )
+
+func testAccPreCheck(t *testing.T) {
+	// You can add code here to run prior to any test case execution, for example assertions
+	// about the appropriate environment variables being set are common to see in a pre-check
+	// function.
+}
--- main.go
+++ main.go
@@ -2,17 +2,44 @@ package main
 
 import (
 	"context"
+	"flag"
+	"log"
 	"terraform-provider-keboola/keboola"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
-// Provider documentation generation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-name keboola
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ./examples/
+
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary.
+	version string = "dev"
+
+	// goreleaser can pass other information to the main package, such as the specific commit
+	// https://goreleaser.com/cookbooks/using-main.version/
+)
 
 func main() {
-	_ = providerserver.Serve(context.Background(), keboola.New, providerserver.ServeOpts{
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+	err := providerserver.Serve(context.Background(), keboola.New(version), providerserver.ServeOpts{
 		Address: "registry.terraform.io/keboola/keboola",
+		Debug:   debug,
 	})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
 }
