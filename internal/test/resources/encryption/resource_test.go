be213e0db253b64817ffd089fd6374ca385fa1fe
Jáchym Toušek <jachym.tousek@keboola.com>
2025-05-13 10:20:33 +0200

    fix: Rename package

--- internal/test/resources/encryption/resource_test.go
+++ internal/test/resources/encryption/resource_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
-	"github.com/keboola/terraform-provider-keboola/internal/provider_test"
+	"github.com/keboola/terraform-provider-keboola/internal/test"
 )
 
 // For running the tests, we'll need a provider factory setup which will be defined in the provider package
@@ -16,12 +16,12 @@ import (
 func TestAccEncryptionResource(t *testing.T) {
 	t.Parallel()
 	resource.Test(t, resource.TestCase{
-		ProtoV6ProviderFactories: provider_test.TestAccProtoV6ProviderFactories(),
-		PreCheck:                 provider_test.TestAccPreCheck,
+		ProtoV6ProviderFactories: test.TestAccProtoV6ProviderFactories(),
+		PreCheck:                 test.TestAccPreCheck,
 		Steps: []resource.TestStep{
 			// Create and Read testing
 			{
-				Config: provider_test.ProviderConfig() + testEncryptionResourceConfig("valuetoencrypt"),
+				Config: test.ProviderConfig() + testEncryptionResourceConfig("valuetoencrypt"),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("keboola_encryption.test", "value", "valuetoencrypt"),
 					resource.TestMatchResourceAttr("keboola_encryption.test", "encrypted_value", regexp.MustCompile(`KBC::ProjectSecure.*::.+`)),
@@ -31,7 +31,7 @@ func TestAccEncryptionResource(t *testing.T) {
 			},
 			// Update and Read testing
 			{
-				Config: provider_test.ProviderConfig() + testEncryptionResourceConfig(""),
+				Config: test.ProviderConfig() + testEncryptionResourceConfig(""),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("keboola_encryption.test", "value", ""),
 					resource.TestMatchResourceAttr("keboola_encryption.test", "encrypted_value", regexp.MustCompile(`KBC::ProjectSecure.*::.+`)),
